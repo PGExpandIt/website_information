5582c2f27b1b7effe55d030cd42b3b9b4e1cba53
Piotr Gajownik <gajownikp@gmail.com>
2025-05-13 21:59:24 +0200

    Create website

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 interface CommonStyles {
     container: string;
@@ -10,7 +11,7 @@ const COMMON_STYLES: CommonStyles = {
 
 const Header = () => (
     <header className={`flex items-center justify-center mb-12 ${COMMON_STYLES.container}`}>
-        <img src={`/logo.png`} alt="Company Logo" width={120} height={120} className="mr-4" />
+        <Image src={`/logo.png`} alt="Company Logo" width={120} height={120} className="mr-4" />
         <div>
             <h1 className="text-4xl font-bold text-white">Piotr Gajownik - ExpandIt</h1>
             <p className="text-lg mt-2 text-gray-200 text-center">
